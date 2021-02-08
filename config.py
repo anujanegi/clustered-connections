9c698a724502efec743076d12f553f79e01cc686
anujanegi <anujanegi1@gmail.com>
2021-02-08 19:35:40 +0100

    init

---
+++ config.py
@@ -0,0 +1,59 @@
+from brian2.units import *
+
+N_realizations = 12
+N_trials = 9
+
+N_exc = 4000
+N_inh = 1000 
+N_split = 1600
+
+dt = 0.1*ms
+duration1 = 1.5*second
+duration2 = 2*second
+
+V = {
+    'v_threshold': 1.,
+    'v_reset': 0.
+}
+
+taus = {
+    'refractory_period': 5*ms,
+    'tau_exc': 15*ms,
+    'tau_inh': 10*ms,
+    'tau_2_exc': 3*ms,
+    'tau_2_inh': 2*ms,
+    'tau_1': 1*ms
+    }
+
+mus = {
+    'mu_exc_low': 1.1,
+    'mu_exc_high': 1.2,
+    'mu_inh_low': 1.,
+    'mu_inh_high': 1.05
+    }
+
+J = {
+    'J_EE': 0.024,
+    'J_EI': -0.045,
+    'J_IE': 0.014,
+    'J_II': -0.057,
+    'scale': 1.9
+    }
+
+P_uniform = {
+    'p_EE': 0.2,
+    'P_EI': 0.5,
+    'P_IE': 0.5,
+    'P_II': 0.5
+    }
+
+
+equations = '''
+        dv/dt = (mu - v)/tau_m + F: 1 (unless refractory)
+        dx/dt = -x/tau_2 : 1
+        dF/dt = ((x/tau_2)-F)/tau_1 : Hz
+        mu : 1
+        tau_m : second
+        tau_1 : second
+        tau_2 : second
+        '''
---
+++ network.py
@@ -0,0 +1,142 @@
+import setuptools
+import numpy as np
+from brian2 import *
+prefs.codegen.target = "numpy"
+
+class Network():
+    
+    def __init__(self, equations, N_exc, N_inh, V, taus, mus, synaptic_strengths, probabilities, N_cluster=None, is_cluster=True, method='euler'):
+        """
+        Initialises network parameters.
+        :param equations: differential equations defining the network
+        :param is_cluster: Boolean to define if the network is clustered or not
+        :param method: numerical integration method
+        """
+        self.equations = equations
+        self.N_exc = N_exc
+        self.N_inh = N_inh
+        self.N_cluster = N_cluster
+
+        self.v_threshold = V['v_threshold']
+        self.v_reset = V['v_reset']
+
+        self.refractory_period = taus['refractory_period']
+        self.tau_exc = taus['tau_exc']
+        self.tau_inh = taus['tau_inh']
+        self.tau_2_exc = taus['tau_2_exc']
+        self.tau_2_inh = taus['tau_2_inh']
+        self.tau_1 = taus['tau_1']
+
+        self.mu_exc_low = mus['mu_exc_low']
+        self.mu_exc_high = mus['mu_exc_high']
+        self.mu_inh_low = mus['mu_inh_low']
+        self.mu_inh_high = mus['mu_inh_high']
+
+        self.synaptic_strengths = synaptic_strengths
+        self.probabilities = probabilities
+
+        self.is_cluster = is_cluster
+        self.method = method
+
+    def initialise_neuron_group(self, n_neurons, mu_high, mu_low, tau_m, tau_2, v_threshold=None, v_reset=None, refractory_period=None, tau_1=None, dt='0.1*ms', voltage='rand()'):
+        """
+        Initialises a group of neurons.
+        : param n_neurons: number of neurons in the group
+        : param v_threshold: voltage threshold value
+        : param v_reset: voltage reset value
+        : param refractory_period: length of the refractory period
+        : param mu_high: Higher limit of mu value
+        : param mu_low: Lower limit of mu value
+        : param tau_m: tau_m value
+        : param tau_1: tau_1 value
+        : param tau_2: tau_2 value
+        : param dt: the time step
+        : param voltage: initial voltage value
+        : return: initialised neuron group
+        """
+
+        if v_threshold is None: v_threshold=self.v_threshold
+        if v_reset is None: v_reset=self.v_reset
+        if refractory_period is None: refractory_period=self.refractory_period
+        if tau_1 is None: tau_1=self.tau_1
+
+        neuron_group = NeuronGroup(n_neurons, self.equations, threshold='v>%f'%v_threshold, reset='v=%f'%v_reset, refractory=refractory_period, method=self.method)
+        neuron_group.mu = np.random.uniform(mu_low, mu_high, size=n_neurons)
+        neuron_group.tau_m = tau_m
+        neuron_group.tau_1 = tau_1
+        neuron_group.tau_2 = tau_2
+        neuron_group.v = voltage
+
+        return neuron_group
+
+    def connect(self, source, destination, synaptic_strength, probability, condition='i!=j'):
+        """
+        Creates a synaptic connection between neuron groups.
+        : param source: source neuron group
+        : param destination: destination neuron group
+        : param synaptic_strength: synaptic strength to update after every pre-synaptic spike
+        : param probability: probability of forming a connection
+        : param condition: any conditions for forming a connection
+        """
+        
+        on_pre_ = 'x_post += %f'%synaptic_strength
+        synapse = Synapses(source, destination, on_pre=on_pre_)
+        synapse.connect(condition=condition, p = probability)
+
+        return synapse
+
+    def build_network(self, neuron_group_a, neuron_group_b, synaptic_strengths, probabilities, is_cluster, N_cluster=None, scale_synaptic_strength=1):
+        """
+        Forms connections between neuron groups based on if or not a cluster
+        """
+        ss = list(synaptic_strengths.values())
+        p = list(probabilities.values())
+
+        if(is_cluster):
+            # intra cluster
+            intra_cluster_condition = 'i!=j and (floor(i/N_cluster)==floor(j/N_cluster))'
+            self.connect(neuron_group_a, neuron_group_a, ss[0]*scale_synaptic_strength, p[0][0], intra_cluster_condition)
+            # inter cluster
+            inter_cluster_condition = 'i!=j and not (floor(i/N_cluster)==floor(j/N_cluster))'
+            self.connect(neuron_group_a, neuron_group_a, ss[0], p[0][1], inter_cluster_condition)
+        else:
+            self.connect(neuron_group_a, neuron_group_a, ss[0], p[0])
+        
+        self.connect(neuron_group_b, neuron_group_a, ss[1], p[1])
+        self.connect(neuron_group_a, neuron_group_b, ss[2], p[2])
+        self.connect(neuron_group_b, neuron_group_b, ss[3], p[3])
+
+    def run_network(self, duration_1, duration_2=1, N_realizations=1, N_trials=1,  N_split=1, monitor=True):
+        """
+        Runs a simulation of the network while monitoring it
+        """
+        start_scope()
+        spike_train_realization = []
+
+        for realization in range(N_realizations):
+
+            excitatory = self.initialise_neuron_group(n_neurons=self.N_exc, mu_high=self.mu_exc_high, mu_low=self.mu_exc_low, tau_m=self.tau_exc, tau_2=self.tau_2_exc)
+            inhibitatory = self.initialise_neuron_group(n_neurons=self.N_inh, mu_high=self.mu_inh_high, mu_low=self.mu_inh_low, tau_m=self.tau_inh, tau_2=self.tau_2_inh)
+            if self.is_cluster:
+                self.build_network(excitatory, inhibitatory, self.synaptic_strengths, self.probabilities, self.is_cluster, self.N_cluster, self.synaptic_strengths['scale'])
+            else:
+                self.build_network(excitatory, inhibitatory, self.synaptic_strengths, self.probabilities, self.is_cluster)
+
+            run(duration_1)
+
+            if monitor:
+                excitatory_split = excitatory[:N_split]
+                state_monitor_excitatory = StateMonitor(excitatory, 'v', record=True)
+                spike_monitor_excitatory = SpikeMonitor(excitatory_split)
+            
+            store()
+
+            spike_train_trials = []
+            for trial in range(N_trials):
+                restore()
+                run(duration_2)
+                spike_train_trials.append(spike_monitor_excitatory.spike_trains())
+
+            spike_train_realization.append(spike_train_trials)
+
+        return spike_train_realization
